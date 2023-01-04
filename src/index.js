74f49ca19ae479eb13cc2e7957f5a7a25a5c5c9e
Vlad1kun <viparrow@mail.ru>
2023-01-04 20:39:16 +0300

    init: work_js_6

--- src/index.js
+++ src/index.js
@@ -5,20 +5,22 @@ const nums4 = 'hundred';
 
 
 module.exports = function toReadable (number) {
-    number = number.toString()
-    if (number == 0) {
-      return 'zero';
-    } else if (number < 10) {
-      return nums1[number];
-    } else if (number < 20) {
-      return nums2[number % 10];
-    } else if (number < 100) {
-      return nums3[(number - (number % 10))/10] + ' ' + nums1[number % 10]
-    } else if (number < 1000 && number[1] < 2) {
-      return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums2[(number % 100) % 10]
-    } else if (number < 1000 && number[1] >= 2 && number[2] > 0) {
-      return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums3[((number % 100) - (number % 10))/10] + ' ' + nums1[number % 10]
-    } else if (number < 1000 && number[1] >= 2 && number[2] == 0) {
-      return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums3[((number % 100) - (number % 10))/10]
-    }
+  number = number.toString()
+  if (number == 0) {
+    return 'zero';
+  } else if (number < 10) {
+    return nums1[number];
+  } else if (number < 20) {
+    return nums2[number % 10];
+  } else if (number < 100) {
+    return nums3[(number - (number % 10))/10] + ' ' + nums1[number % 10]
+  } else if (number < 1000 && number[1] < 1) {
+    return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums1[(number % 100) % 10]
+  } else if (number < 1000 && number[1] == 1) {
+    return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums2[(number % 100) % 10]
+  } else if (number < 1000 && number[1] >= 2 && number[2] > 0) {
+    return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums3[((number % 100) - (number % 10))/10] + ' ' + nums1[number % 10]
+  } else if (number < 1000 && number[1] >= 2 && number[2] == 0) {
+    return nums1[(number - (number % 100))/ 100] + ' ' + nums4 + ' ' + nums3[((number % 100) - (number % 10))/10]
+  }
 }
